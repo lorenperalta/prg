677e76f3a4618ba46ca8146baefb81113946f747
Loren Peralta <stinpc@hotmail.com>
2020-11-04 21:49:59 -0500

    mvc del prg

---
+++ Models/Registro.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class Registro
+    {
+        public string nombre { get; set; }
+        public double precio { get; set; }
+        public string descripcion { get; set; }
+    }
+}
